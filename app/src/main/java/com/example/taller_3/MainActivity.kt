e277974dd5df2d3ba4a3e37f01a921d9462df49c
Rodrivdlc <rodri.vdlc@gmail.com>
2024-11-08 18:54:27 +0100

    imports

--- app/src/main/java/com/example/taller_3/MainActivity.kt
+++ app/src/main/java/com/example/taller_3/MainActivity.kt
@@ -17,6 +17,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.taller_3.GreetingAsyncTask
+import com.example.taller_3.SettingsScreen
 import com.example.taller_3.ui.theme.Taller_3Theme
 import java.util.*
 
