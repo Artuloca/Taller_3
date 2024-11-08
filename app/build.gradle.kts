49f615ed33ae5d338f5d5991e89e42a72959d095
Arturo <artuloca@myuax.co>
2024-11-08 20:13:07 +0100

    cambios

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation(libs.androidx.loader)
+    implementation(libs.androidx.room.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
